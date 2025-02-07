3247827ae76286a0f7b44b0952b03fff3bf2e284
khalil moro <MY_NAME@example.com>
2025-02-07 21:16:47 +0100

    to push

--- backend/index.js
+++ backend/index.js
@@ -115,7 +115,7 @@ app.post("/login", async (req, res) => {
   
       const token = jwt.sign(
         { userId: user.id, username: user.username },
-        'secret',
+        process.env.SECRET,
         // { expiresIn: '1h' }
       )
       res.cookie('jwt', token, {
@@ -138,7 +138,7 @@ app.post("/logout", (req, res)=>{
 app.get("/user", async(req, res)=>{
     try{
         const cookie = req.cookies['jwt']
-        const claims = jwt.verify(cookie, 'secret')
+        const claims = jwt.verify(cookie, process.env.SECRET)
         if (!claims){
             return res.status(404).send({message:"unauthenticated"})
         }
